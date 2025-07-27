dfd5b0275e656771d0ae46fa9eb0cc8296da0739
Tu Nombre <meibyburgosa@gmail.com>
2025-07-27 00:58:41 -0400

    implementando UserDetailsService en clase modelo Usuario y modificando clase AutenticacionService

--- src/main/java/com/alura/forohub/model/Usuario.java
+++ src/main/java/com/alura/forohub/model/Usuario.java
@@ -6,6 +6,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 @EqualsAndHashCode(of ="id")
 
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -63,4 +64,4 @@ public class Usuario {
     }
 }
 
-}
+
