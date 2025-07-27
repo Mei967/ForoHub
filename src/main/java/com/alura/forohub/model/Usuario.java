14ca69b0561c570bbf4bdf1683a5732bf797d80a
Tu Nombre <meibyburgosa@gmail.com>
2025-07-26 23:05:01 -0400

    Implementando la interfaz UserDetails en clase Usuraio para que spring security pueda usarla en elm proceso de auenticación Usuario

--- src/main/java/com/alura/forohub/model/Usuario.java
+++ src/main/java/com/alura/forohub/model/Usuario.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "usuarios")
 @Entity(name = "Usuario")
@@ -20,5 +24,43 @@ public class Usuario {
     private Long id;
     private String login;
     private String clave;
+
+    //Implementación de la interfaz UserDetails
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(); // No tenemos roles por ahora
+    }
+
+    @Override
+    public String getPassword() {
+        return clave;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
 
 }
